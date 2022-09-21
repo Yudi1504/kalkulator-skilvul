56b969678fe010ada88c0290e47795cc9055ed34
Yudi1504 <synyster@student.untan.ac.id>
2022-09-21 23:54:42 +0700

    21-Sep-22

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SkilvulLearning
+{
+    class Latihan
+    {
+        static void Main(string[] args)
+        {
+            double angkaPertama = 0;
+            double angkaKedua = 0;
+
+            double hasil = 0;
+            Console.WriteLine("Program Kalkulator Sederhana");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.Write("Masukkan Angka Pertama : ");
+            angkaPertama = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan Angka Kedua : ");
+            angkaKedua = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Tekan 1 untuk Pertambahan(+)");
+            Console.WriteLine("Tekan 2 untuk Pengurangan(-)");
+            Console.WriteLine("Tekan 3 untuk Perkalian(x)");
+            Console.WriteLine("Tekan 4 untuk Pembagian(:)");
+            Console.WriteLine();
+
+            switch (Console.ReadLine()){
+                case "1":
+                    hasil = angkaPertama + angkaKedua;
+                    Console.WriteLine($"Hasil pertambahannya adalah: {angkaPertama} + {angkaKedua} = " + hasil);
+                    break;
+                case "2":
+                    hasil = angkaPertama - angkaKedua;
+                    Console.WriteLine($"Hasil pengurangannya adalah: {angkaPertama} - {angkaKedua} = " + hasil);
+                    break;
+                case "3":
+                    hasil = angkaPertama * angkaKedua;
+                    Console.WriteLine($"Hasil perkaliannya adalah: {angkaPertama} x {angkaKedua} = " + hasil);
+                    break;
+                case "4":
+                    hasil = angkaPertama / angkaKedua;
+                    Console.WriteLine($"Hasil pembagiannya adalah: {angkaPertama} : {angkaKedua} = " + hasil);
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
